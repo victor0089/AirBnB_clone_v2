17bedbb5d5765ae017bc8d3ac13159d6c384dc7c
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-26 12:12:49 +0200

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -47,23 +47,23 @@ test_db_storage.py'])
 
 
  class TestFileStorage(unittest.TestCase):
-     """Test the FileStorage class"""
-     @unittest.skipIf(storage_t != 'db', "not testing db storage")
-     def test_all_returns_dic(self):
-         """Test that all returns a dictionaty"""
-        self.assertIs(type(models.storage.all()), dic)
+    """Test the FileStorage class"""
+    @unittest.skipIf(storage_t != 'db', "not testing db storage")
+    def test_all_returns_dict(self):
+        """Test that all returns a dictionary"""
+        self.assertIs(type(models.storage.all()), dict)
 
-     @unittest.skipIf(storage_t != 'db', "not testing db storage")
-     def test_all_no_class(self):
-         """Test that all returns all rows when no class is passed"""
+    @unittest.skipIf(storage_t != 'db', "not testing db storage")
+    def test_all_no_class(self):
+        """Test that all returns all rows when no class is passed"""
 
-     @unittest.skipIf(storage_t != 'db', "not testing db storage")
-     def test_new(self):
-         """test that new adds an object to the database"""
+    @unittest.skipIf(storage_t != 'db', "not testing db storage")
+    def test_new(self):
+        """Test that new adds an object to the database"""
 
-     @unittest.skipIf(storage_t != 'db', "not testing db storage")
-     def test_save(self):
-         """Test that save properly saves objects to file.json"""
+    @unittest.skipIf(storage_t != 'db', "not testing db storage")
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
 
 class TestDBStorageDocs(unittest.TestCase):
     """Tests to check the documentation and style of DBStorage class"""
