19dd001538b6c870fa7b2e6b7adec72adfea1399
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-01-04 10:52:02 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -39,4 +39,3 @@ class TestConsoleDocs(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
-               
